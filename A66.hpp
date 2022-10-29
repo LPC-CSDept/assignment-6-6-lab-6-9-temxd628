82ba69876df65f9b3dbabfc231ecef9626fe9b97
Tem <dxpetrenko840@zonemail.clpccd.edu>
2022-10-28 20:07:32 -0700

    find min and max

--- A66.hpp
+++ A66.hpp
@@ -4,4 +4,24 @@
 #include <iostream>
 using namespace std;
 
+void getinput(int &a, int &b, int &c){
+    cin >> a >> b >> c;
+}
+
+void maxmin(int a, int b, int c, int &max, int &min) {
+    if (a < b && a < c)
+        min = a; 
+    else if (b < c)
+        min = b;
+    else 
+        min = c;
+    
+    if (a > b && a > c)
+        max = a; 
+    else if (b > c)
+        max = b;
+    else 
+        max = c;
+}
+
 // complete the function maxmin( )
